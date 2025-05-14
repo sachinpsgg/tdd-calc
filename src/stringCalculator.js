c6a643fadcc7a314ab5176f19d7277b4ffbe0d8b
sachinpsgg <sachin@psglobalgroup.com>
2025-05-14 18:13:25 +0530

    test case 6 for handling negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -11,6 +11,12 @@ function add(numbers) {
     }
 
     const numArray = numbers.split(new RegExp(`[\\n${delimiter}]`)).map(Number);
+
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
+    }
+
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 module.exports = add;
